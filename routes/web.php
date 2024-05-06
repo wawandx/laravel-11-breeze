1f9b869b752b2cf90bd77f1d95c6cd100dfa1b19
Rachmad Kurniawan <wawandx@gmail.com>
2024-05-06 10:10:15 +0800

    breeze and integration email with mailpit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', HomeController::class)->name('home');
+
+Route::get('/dashboard', function () {
+    return view('dashboard', ['title' => 'Dashboard']);
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
